fc6934dc084ac0fcdbb178d24b6533ddf08733a5
ariendesitakarina <riechandtz@gmail.com>
2016-03-29 19:09:50 +0700

    Added files via upload

---
+++ OperatorKuadrat.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operatorkuadrat;
+
+/**
+ *
+ * @author ARIEN
+ */
+public class OperatorKuadrat {
+
+    static int OperatorKuadrat(int bil){
+     return bil*bil;
+ }
+    public static void main(String[] args) {
+       int z=10;
+       System.out.println(z +" Kuadrat yaitu "+ OperatorKuadrat(z));
+    }
+    
+}
