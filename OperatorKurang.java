916e542ce8b3e9da7780a68620677f308bd3f931
ariendesitakarina <riechandtz@gmail.com>
2016-03-29 11:51:12 +0700

    Added files via upload

---
+++ OperatorKurang.java
@@ -0,0 +1,13 @@
+public class OperatorKurang{
+
+public static void main (String args[]){
+
+int bil1,bil2,hsl;
+
+bil1=9;
+bil2=3;
+hsl=bil1-bil2;
+
+System.out.println(bil1+"-"+bil2+"="+hsl);
+}
+}
---
+++ SomethingMethod.java
@@ -0,0 +1,25 @@
+public class SomethingMethod {
+
+public static void main (String[] args) {
+
+SomethingMethod nama = new SomethingMethod();
+System.out.println(nama.namaSaya());
+nama.pertambahan();
+
+
+}
+
+//fungsi -> sesuatu yang mengembalikan nilai
+public String namaSaya(){
+return "Arien";
+}
+
+public void pertambahan() {
+int a = 1;
+int b = 2;
+System.out.println(a+b);
+}
+
+}
+
+
