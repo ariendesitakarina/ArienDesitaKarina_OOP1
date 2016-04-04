d0c767ed9bcd963b55838986c2e1369ca40408e6
ariendesitakarina <riechandtz@gmail.com>
2016-04-04 17:51:49 +0700

    Added files via upload

---
+++ Arien_array1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arien_array1;
+
+/**
+ *
+ * @author ARIEN
+ */
+public class Arien_array1 {
+
+
+    public static void main(String[] args) {
+         int[] x= new int[3];
+        
+        x[2]=21;
+        x[1]=22;
+        x[0]=23;
+        System.out.println(x[0]);
+        System.out.println(x[1]);
+        System.out.println(x[2]);        
+        
+    }
+    
+}
