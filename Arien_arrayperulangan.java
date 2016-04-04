01d6c73285cbee127455f8cba64430a456103190
ariendesitakarina <riechandtz@gmail.com>
2016-04-04 17:52:25 +0700

    Added files via upload

---
+++ Arien_arrayperulangan.java
@@ -0,0 +1,14 @@
+package arien_arrayperulangan;
+
+public class Arien_arrayperulangan {
+
+    public static void main(String[] args) {
+                        
+        int [] x= new int[]{};
+        
+        for (int i=1; i<21; i++){
+            System.out.println(i);
+        }
+    }
+    
+}
