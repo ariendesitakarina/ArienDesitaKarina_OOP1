cc7700bcf603299700e2adbd4cda6691963d1594
ariendesitakarina <riechandtz@gmail.com>
2016-04-05 07:54:52 +0700

    Added files via upload

---
+++ Arien_coba4.java
@@ -0,0 +1,14 @@
+package arien_coba4;
+
+public class Arien_coba4 {
+
+    public static void main(String[] args) {
+        int i=0;
+        do
+        {
+        System.out.println("i is :" +i);
+        i++;
+        
+        } while(i < 21);
+    }
+}
