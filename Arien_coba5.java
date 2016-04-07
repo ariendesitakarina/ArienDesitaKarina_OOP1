4ebd818eb1afc9babd8ce5b51244eef78b358c53
ariendesitakarina <riechandtz@gmail.com>
2016-04-07 07:04:47 +0700

    Added files via upload

---
+++ Arien_coba5.java
@@ -0,0 +1,13 @@
+package arien_coba5;
+
+public class Arien_coba5 {
+
+    public static void main(String[] args) {
+        int c;
+        
+        for (c = 1; c <= 13; c++) {
+            System.out.println(c);
+        }
+    }
+    
+}
